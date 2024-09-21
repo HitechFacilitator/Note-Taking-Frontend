430f51010530fe2bb0e0b2bda3f52b47ff5df989
HitechAngel <hitechangel@localhost.localdomain>
2024-09-21 13:39:45 +0100

    Loading, error and empty notes handler : Established

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Spinner } from "react-bootstrap";
 import { FaPlus } from "react-icons/fa";
 import * as Api from "./Network/notes.api";
 //importing Components
@@ -15,16 +15,22 @@ function App() {
   // Initialising the parameters for the AddNote form/box/modal
   const [showAddNote, setShowAddNote] = useState(false)
   const [noteToUpdate, setNoteToUpdate] = useState(null)
+  const [notesLoading, setnotesLoading] = useState(true)
+  const [showNoteLoadingError, setShowNoteLoadingError] = useState(false)
 
   useEffect(() =>{
     //  Async function to fetch all notes 
     async function getAllNotes() {
       try {
+          setnotesLoading(true)
+          setShowNoteLoadingError(false)
           const response = await Api.fetchAllData()
           setNotes(response)
         } catch (error) {
           console.error("Error in loading all Notes : ",error);
-          alert(error);
+          setShowNoteLoadingError(true)
+        } finally {
+          setnotesLoading(false)
         }
     }
     getAllNotes() //Function call
@@ -51,9 +57,23 @@ function App() {
     }
   }
 
+  // a const to display the notes to facilitate handling in the return fxn
+  const displayNotes = <Row xs={1} md={2} xl={3} xxl={4} >{/* 1 row sor small size screen,2 for medium, 3 for large and 4 for extra large screen*/}
+                          {notes.map(note =>(
+                            <Col key={note._id} className="mb-4">
+                              <Note 
+                                note={note}
+                                handleDelete={deleteNote}
+                                onNoteClick={setNoteToUpdate}
+                              />
+                            </Col>
+                          ))}
+                        </Row>
+
   return (
     // displaying the notes in a grid container
-    <Container>
+    <Container >
+
       <Button onClick={() => {
           setShowAddNote(true)
         }}
@@ -62,17 +82,18 @@ function App() {
         <FaPlus/>
         Add a Note 
       </Button>
-      <Row xs={1} md={2} xl={3} xxl={4} >{/* 1 row sor small size screen,2 for medium, 3 for large and 4 for extra large screen*/}
-        {notes.map(note =>(
-          <Col key={note._id} className="mb-4">
-            <Note 
-              note={note}
-              handleDelete={deleteNote}
-              onNoteClick={setNoteToUpdate}
-            />
-          </Col>
-        ))}
-      </Row>
+      
+      {/* establishing the spinner, error handler and empty handler */}
+      {notesLoading && <Spinner animation="border" variant="primary" className={`${Utilstyles.blockCenter}`}/>}
+      {showNoteLoadingError && <p className={Utilstyles.center}> An Error was encountered when fetching all notes </p>}
+      {!notesLoading && !showNoteLoadingError &&
+        <>
+          {
+            notes.length > 0 ? displayNotes : <p className={Utilstyles.center}>You have no Notes yet</p>
+          }
+        </>
+      }
+
       { showAddNote &&
         <AddNoteBox 
           handleClose={close} 
